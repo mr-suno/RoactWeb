7eedf3deb04c85e2387514642beb8cfb641b2ede
Suno <ilovesuno@proton.me>
2025-07-01 18:56:48 -0700

    Remove Roact directory

--- Roact/PropMarkers/Children.lua
+++
@@ -1,5 +0,0 @@
-local Symbol = getgenv().require("Symbol")
-
-local Children = Symbol.named("Children")
-
-return Children
--- Roact/PropMarkers/Ref.lua
+++
@@ -1,5 +0,0 @@
-local Symbol = getgenv().require("Symbol")
-
-local Ref = Symbol.named("Ref")
-
-return Ref
