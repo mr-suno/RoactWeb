583f310281138ee1295949797083738402889c7b
Suno <ilovesuno@proton.me>
2025-07-01 18:37:15 -0700

    Remove old Roact directory

--- Roact/GlobalConfig.lua
+++
@@ -1,7 +0,0 @@
---[[
-	Exposes a single instance of a configuration as Roact's GlobalConfig.
-]]
-
-local Config = getgenv().require("Config")
-
-return Config.new()
--- Roact/Portal.lua
+++
@@ -1,5 +0,0 @@
-local Symbol = getgenv().require("Symbol")
-
-local Portal = Symbol.named("Portal")
-
-return Portal
